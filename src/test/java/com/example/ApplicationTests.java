8f73458b7857bfc98ff62c3cf835791a3d59ef2b
heowc <heowc1992@gmail.com>
2021-11-27 13:01:06 +0900

    Add test

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,27 +1,40 @@
 package com.example;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
 import org.hibernate.exception.SQLGrammarException;
 import org.hibernate.jpa.TypedParameterValue;
 import org.hibernate.type.LongType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @Transactional
 @SpringBootTest
 @Testcontainers(disabledWithoutDocker = true)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApplicationTests {
 
     @Autowired
     private EntityManager entityManager;
+    @Autowired
+    private DataSource dataSource;
 
     @BeforeEach
     void beforeEach() {
@@ -70,5 +83,62 @@ class ApplicationTests {
                                          .setParameter("count", null)
                                          .setParameter("id", id);
         query.executeUpdate();
+    }
+
+
+    @Test
+    void prepareStatement() throws SQLException {
+        assertThatThrownBy(() -> {
+            final Long id = 1L;
+            try (final Connection connection = dataSource.getConnection();
+                 final PreparedStatement ps = connection.prepareStatement("UPDATE message SET count = ? WHERE id = ?")) {
+                ps.setNull(1, Types.BINARY);
+                ps.setLong(2, id);
+                ps.executeUpdate();
+            }
+        })
+        .hasMessageContaining(
+                "ERROR: column \"count\" is of type bigint but expression is of type bytea\n" +
+                "  Hint: You will need to rewrite or cast the expression.\n" +
+                "  Position: 28");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void solvedPrepareStatement(Consumer<PreparedStatement> consumer)
+            throws SQLException {
+        final Long id = 1L;
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement ps = connection.prepareStatement("UPDATE message SET count = ? WHERE id = ?")) {
+            consumer.accept(ps);
+            ps.setLong(2, id);
+            ps.executeUpdate();
+        }
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+            Arguments.of((Consumer<PreparedStatement>) ps -> {
+                try {
+                    ps.setNull(1, Types.INTEGER);
+                } catch (SQLException e) {
+                    // ignored
+                }
+            }),
+            Arguments.of((Consumer<PreparedStatement>) ps -> {
+                try {
+                    ps.setNull(1, Types.BINARY, "int8");
+                } catch (SQLException e) {
+                    // ignored
+                }
+            }),
+            Arguments.of((Consumer<PreparedStatement>) ps -> {
+                try {
+                    ps.setNull(1, Types.BINARY, "null"); // Not recommend
+                } catch (SQLException e) {
+                    // ignored
+                }
+            })
+        );
     }
 }
